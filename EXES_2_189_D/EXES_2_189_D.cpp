58cb39ac800f85bb265183bf610ae425f0a7867e
salss33 <hallosalsa03@gmail.com>
2023-05-12 10:35:04 +0700

    Add project files.

---
+++ EXES_2_189_D/EXES_2_189_D.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+int salsa[mid]; // Array to be search
+int n; // set Salsa
+int i; // set Bila
+
+void input()
+{
+    while (true);
+}
+
+// Accept array elemnts
+
+void input;
+{
+    char ch;
+    number of comparison
+
+do
+{
+	// Accept the number to be search
+	cout << "\nEnter the element you want to search: "; // 
+	cin >> item;
+
+	comparison = 0;
+	for                              // 
+		
+			if desired element<=[mid]
+
+     
